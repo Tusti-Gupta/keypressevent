533ed84a55c89ca03051f5d91c4fd027295861aa
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2022-01-10 10:16:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
-img_width = 200;
-img_height = 100;
+img_width = 500;
+img_height = 300;
 
 var img_image;
 
@@ -27,12 +27,29 @@ function my_keydown(e)
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
 	
-		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
-			document.getElementById("d1").innerHTML = "You pressed an alphabet key";
-			console.log("alphabet key");
-			alphabetkey();
-		}
+	if ((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
+		document.getElementById("d1").innerHTML = "You pressed an alphabet key";
+		console.log("alphabet key");
+		alphabetkey();
+	} else if (keyPressed >=48 && keyPressed<=57) {
+		document.getElementById("d1").innerHTML = "You pressed a number key";
+		console.log("number key");
+		numberkey();
+	} else if (keyPressed >=37 && keyPressed<=40) {
+		document.getElementById("d1").innerHTML = "You pressed an arrow key";
+		console.log("arrow key");
+		arrowkey();
+	} else if ((keyPressed == 17) || (keyPressed == 18) || (keyPressed == 27)) {
+		document.getElementById("d1").innerHTML = "You pressed a special key";
+		console.log("special key");
+		specialkey();	
+	} else {
+		document.getElementById("d1").innerHTML = "You pressed some other key";
+		console.log("other key");
+		otherkey();
+	}
 }
+
 
 function alphabetkey() {
 	img_image = "Alpkey.png";
